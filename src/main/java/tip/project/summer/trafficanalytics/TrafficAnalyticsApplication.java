f199210a0d5902a89e00c3d0b67c7b0dc5f67c33
zarasuto <zarasuto@gmail.com>
2022-07-02 11:29:34 +0800

    Initial commit

---
+++ src/main/java/tip/project/summer/trafficanalytics/TrafficAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package tip.project.summer.trafficanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficAnalyticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tip/project/summer/trafficanalytics/TrafficAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package tip.project.summer.trafficanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
